c88b62409eda10914bcb4f986a437d2253bb572d
SamuelRicardoDS <samuelricardods@gmail.com>
2022-03-29 14:21:06 -0300

    rotas feitas

---
+++ src/pages/Curriculum/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Curriculum() {
+    return(
+        <div>
+        <div>SAMUEL</div>
+        <button>
+                <Link to="/">retornar à pagina inicial</Link>
+        </button>
+        </div>
+    )
+}
+
+export default Curriculum
